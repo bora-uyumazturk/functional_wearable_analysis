0a0c87c2ca321d37415e1722288ae4dfb4e264a1
Spare <spare@Landing-C02W64RAHV2L.fios-router.home>
2020-03-25 20:57:41 -0400

    parallelized matrix.py

--- src/data_processing/matrix.py
+++ src/data_processing/matrix.py
@@ -5,6 +5,8 @@ Usage: contains Matrix_Creator class that takes in csv list and merges time data
 """
 import pandas as pd
 from tqdm import tqdm
+import concurrent.futures
+import threading
 
 class Matrix_Creator:
 
@@ -18,28 +20,40 @@ class Matrix_Creator:
         self.csv_list = csv_list
         self.processing_fn = processing_fn
         self.resolution_type = resolution_type
+        self.out = None
+        self.lock = threading.Lock()
+
+    def process_csv(self, csv):
+        df_cur, sid = self.processing_fn(csv, self.resolution_type)
+        self.lock.acquire()
+        print("processing: {}".format(sid))
+        if self.out is None:
+            self.out = df_cur
+            self.out.columns = [sid]
+        else:
+            df_cur.columns = [sid]
+            self.out = self.out.merge(df_cur, how='outer', left_index=True, right_index=True)
+        print("completed: {}".format(sid))
+        self.lock.release() 
 
     def process(self, outpath=None, merge=False):
         """Processes and saves matrix to outpath."""
         print("Processing csvs...")
-        out = None
-        for csv in tqdm(self.csv_list):
-            df_cur, sid = self.processing_fn(csv, self.resolution_type)
-            print(df_cur.head())
-            if out is None:
-                out = df_cur
-                out.columns = [sid]
-            else:
-                df_cur.columns = [sid]
-                out = out.merge(df_cur, how='outer',
-                                left_index=True, right_index=True)
 
-        out = out.T
-        if isinstance(out.columns, pd.MultiIndex):
-            out.columns = range(len(out.columns))
-
+        pool = concurrent.futures.ThreadPoolExecutor(4)
+        future_list = []
+        for csv in self.csv_list:
+            future_list.append(pool.submit(self.process_csv, csv))
+        concurrent.futures.wait(future_list)
+        
+        self.out = self.out.T
+        self.out.sort_index(inplace=True)
+        if isinstance(self.out.columns, pd.MultiIndex):
+            self.out.columns = range(len(self.out.columns))
+       
         if outpath is not None:
-            out.to_csv(outpath)
+            self.out.to_csv(outpath)
             print("Saved matrix to %s." %outpath)
-
-        return out.T
+       
+        return self.out.T
+            
