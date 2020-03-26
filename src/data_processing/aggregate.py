a73283d09a7b51065e84a6129ae682419739fcfa
Spare <spare@Landing-C02W64RAHV2L.fios-router.home>
2020-03-26 02:14:42 -0400

    reorganized data_processing

--- src/data_processing/aggregate.py
+++ src/data_processing/aggregate.py
@@ -1,15 +1,66 @@
 """
-File: make_matrix.py
-Author: Lilia
+File: aggregate.py
+Author: Lilia and Bora
 Usage: --outpath <file to write to>
 	make sure to edit process_fn directly
-"""
+""" 
 import pandas as pd
 import numpy as np
 import argparse
 import os
 
-from matrix import Matrix_Creator
+import concurrent.futures
+import threading
+
+class Aggregator:
+
+    def __init__(self, csv_list, processing_fn, resolution_type):
+        """
+        csv_list : list of csvs to read,
+        processing_fn : processing function with args
+                        csv (csv path), self.resolution_type
+        resolution_type : resolution for processing function
+        """
+        self.csv_list = csv_list
+        self.processing_fn = processing_fn
+        self.resolution_type = resolution_type
+        self.out = None
+        self.lock = threading.Lock()
+
+
+    def process_csv(self, csv):
+        df_cur, sid = self.processing_fn(csv, self.resolution_type)
+        self.lock.acquire()
+        print("processing: {}".format(sid))
+        if self.out is None:
+            self.out = df_cur
+            self.out.columns = [sid]
+        else:
+            df_cur.columns = [sid]
+            self.out = self.out.merge(df_cur, how='outer', left_index=True, right_index=True)
+        print("completed: {}".format(sid))
+        self.lock.release()
+
+
+    def process(self, outpath=None, merge=False):
+        """Processes and saves matrix to outpath."""
+        print("Processing csvs...")
+
+        pool = concurrent.futures.ThreadPoolExecutor(4)
+        future_list = []
+        for csv in self.csv_list:
+            future_list.append(pool.submit(self.process_csv, csv))
+        concurrent.futures.wait(future_list)
+
+        self.out = self.out.T
+        self.out.sort_index(inplace=True)
+        if isinstance(self.out.columns, pd.MultiIndex):
+            self.out.columns = range(len(self.out.columns))
+
+        if outpath is not None:
+            self.out.to_csv(outpath)
+            print("Saved matrix to %s." %outpath)
+
 
 def average_hour_per_day(csv, resolution='hour'):
     """returns df of average accel_magnitude for each hour of day"""
@@ -21,6 +72,7 @@ def average_hour_per_day(csv, resolution='hour'):
         raise(Exception("DataFrame lacks hour columnns"))
     return result, subject_id
 
+
 def average_hour_per_week(csv, resolution='hour'):
     """returns df of average hour per week. assumes csv has
     columns corresponding to 'weekday', 'hour'"""
@@ -30,6 +82,7 @@ def average_hour_per_week(csv, resolution='hour'):
         raise(Exception("DataFrame lacks weekday and/or hour columns"))
     result = pd.DataFrame(df.groupby(["weekday", "hour"]).accel_magnitude.mean()) 
     return result, subject_id
+
 
 def main():
     """writes matrix of average hour activity"""
@@ -54,8 +107,9 @@ def main():
     else:
         raise(Exception("invalid aggregator"))
         return 
-    mtrx = Matrix_Creator(csvs, f, 'hour')
-    mtrx.process(args.outpath, merge=True)
+    aggtr = Aggregator(csvs, f, 'hour')
+    aggtr.process(args.outpath, merge=True)
+
 
 if __name__ == "__main__":
     main()
