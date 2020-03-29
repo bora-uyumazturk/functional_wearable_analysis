cd5f6538e3b0d7e2c99072b532910977cef3a40d
Spare <spare@Landing-C02W64RAHV2L.fios-router.home>
2020-03-29 15:08:04 -0400

    cleaned up fig9/generate_data.py to use dataframe transform utility function

--- paper_code/fig9/generate_data.py
+++ paper_code/fig9/generate_data.py
@@ -2,15 +2,12 @@ import argparse
 import pandas as pd
 
 from models import DFTFactorizer
+from utils import transform_time_series 
 
 def main(source, dest):
     source_df = pd.read_csv(source, index_col=0)
     source_df = source_df.dropna(axis='rows')
-    X = source_df.values
-    factorizer = DFTFactorizer()
-    out = factorizer.transform(X)
-    out_df = pd.DataFrame(out)
-    out_df.index = source_df.index
+    out_df = transform_time_series(source_df, 'dft')
     out_df.to_csv(dest)
 
 if __name__ == '__main__':
--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import pandas as pd
 
+from models import *
 
 def make_directory(dir):
     if not os.path.exists(dir):
@@ -26,3 +27,49 @@ def remove_nans(X, y):
     df.loc[:, 'y'] = y
     df = df.dropna(axis=0)
     return (df.drop(['y'], axis='columns').values, df.loc[:, 'y'].values.reshape((-1, 1)))
+
+
+def get_factorizer(method, num_components=None):
+    """Returns factorizer corresponding to method.
+
+    Args:
+        method (str): String specifying factorization method. One of
+                      ('dft', 'nmf', 'pca', 'kmeans'). 
+        num_components (int): number of components to use for factorizer,
+                              default None.
+    Returns:
+        instance of factorizer corresponding to method.
+    """
+    if method == "dft":
+        return DFTFactorizer(num_components)
+    if method == "nmf":
+        return NMFFactorizer(num_components)
+    if method == "pca":
+        return PCAFactorizer(num_components)
+    if method == "kmeans":
+        return KMeansFactorizer(num_components)
+    raise(Exception("method: {} not supported".format(method)))
+
+
+def transform_time_series(df, method, num_components=None):
+    """Transforms time series using matrix factorization methods.
+
+    Args:
+        df (dataframe): Dataframe holding time series data. Rows
+                        are individuals, columns are samples.
+        method (str): String specifying factorization method. One of
+                      ('dft', 'nmf', 'pca', 'kmeans'). 
+        num_components (int): number of components to use for factorizer,
+                              default None.
+    Returns:
+        Dataframe with same index as df, each column is the latent
+        component of the factorization.
+    """
+    factorizer = get_factorizer(method, num_components)
+    X = df.values
+    factorizer.fit(X)
+    X_tnsfmed = factorizer.transform(X)
+    out_df = pd.DataFrame(X_tnsfmed)
+    out_df.index = df.index
+    return out_df
+    
